999dd05c9220768a413fad666523035a6dec9dfc
신미래 <sml6209@naver.com>
2022-06-16 12:17:16 +0900

    [Lv2] 타겟넘버 수정

--- programmers/mirae/Lv2/타겟 넘버/solution.js
+++ programmers/mirae/Lv2/타겟 넘버/solution.js
@@ -1,10 +1,10 @@
 function solution(numbers, target) {
   let cnt = 0;
 
-  calculate(numbers, target, 0, 0);
+  calculate(0, 0);
 
   // numbers 0인덱스부터 마지막 인덱스까지 계산해보는 재귀함수
-  function calculate(numbers, target, idx, calRes) {
+  function calculate(idx, calRes) {
     // numbers 끝까지 계산했다면 target과 일치하는지 확인
     if (idx === numbers.length) {
       if (calRes === target) cnt++;
@@ -12,9 +12,9 @@ function solution(numbers, target) {
     }
 
     // idx 인덱스의 숫자 더하기
-    calculate(numbers, target, idx + 1, calRes + numbers[idx]);
+    calculate(idx + 1, calRes + numbers[idx]);
     // idx 인덱스의 숫자 빼기
-    calculate(numbers, target, idx + 1, calRes - numbers[idx]);
+    calculate(idx + 1, calRes - numbers[idx]);
   }
 
   return cnt;
